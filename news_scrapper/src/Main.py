ef695f270636cb935466c5a50b6c2f488576fc49
plouto5 <ploutos66p@gmail.com>
2017-06-09 17:21:40 +0000

    cleaned files and created scrape_articles.py

--- news_scrapper/src/Main.py
+++ news_scrapper/src/Main.py
@@ -2,12 +2,11 @@ import soup
 import parse
 
 
-url_list = 'http://mlb.mlb.com/news/article/min/twins-top-orioles-with-ervin-santanas-shutout?ymd=20170523&content_id=231986710&vkey=news_min'
 
-data = soup.make_soup(url_list)
-
-parse.parse_site(data)
-
+def scrape_article(url):
+	data = soup.make_soup(url)
+	x = parse.parse_site(data)
+	return(x)	
 ''' add functions and classes to find the articles
 	to populate the url_list. Then out put to file
 	or email to recipient.
--- news_scrapper/src/soup.py
+++
@@ -1,8 +0,0 @@
-import urllib
-import urllib.request
-from bs4 import BeautifulSoup
-
-def make_soup(url):
-	page = urllib.request.urlopen(url)
-	soupdata = BeautifulSoup(page, "lxml")
-	return soupdata
