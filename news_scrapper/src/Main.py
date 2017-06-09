d64a6edfff969a0d014a97c465e896a05867b027
plouto5 <ploutos66p@gmail.com>
2017-06-09 04:57:27 +0000

    comments to main.py

--- news_scrapper/src/Main.py
+++ news_scrapper/src/Main.py
@@ -8,7 +8,10 @@ data = soup.make_soup(url_list)
 
 parse.parse_site(data)
 
-
+''' add functions and classes to find the articles
+	to populate the url_list. Then out put to file
+	or email to recipient.
+'''
 
 
 
