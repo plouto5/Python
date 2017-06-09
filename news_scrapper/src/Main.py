ed53c9e62fee46a2708b6a160557473226cbdda7
plouto5 <ploutos66p@gmail.com>
2017-06-09 18:08:55 +0000

    cleaning up files

--- news_scrapper/src/Main.py
+++ news_scrapper/src/Main.py
@@ -1,12 +1,16 @@
-import soup
-import parse
+import scrape_articles
 
 
 
-def scrape_article(url):
-	data = soup.make_soup(url)
-	x = parse.parse_site(data)
+
+def scrape(url):
+	data = scrape_articles.make_soup(url)
+	x = scrape_articles.parse_site(data)
 	return(x)	
+
+
+
+
 ''' add functions and classes to find the articles
 	to populate the url_list. Then out put to file
 	or email to recipient.
--- news_scrapper/src/find_articles.py
+++ news_scrapper/src/find_articles.py
@@ -4,19 +4,32 @@ from bs4 import BeautifulSoup
 import datetime
 from datetime import date, timedelta
 
-today = datetime.datetime.today()
-yesterday = datetime.datetime.today() - timedelta(days=1,)#hours=12
-
+#################### Load XML ##########################
 def make_soup(url):
     page = urllib.request.urlopen(url)
     soupdata = BeautifulSoup(page, "xml")
     return soupdata
 
 soup = make_soup('http://mlb.mlb.com/partnerxml/gen/news/rss/min.xml')
+########################################################
 
+#################### Link Scrub ########################
+final_articles = []
 articles = []
 obj_list = []
 
+today = datetime.datetime.today()
+yesterday = datetime.datetime.today() - timedelta(days=1,)#hours=12
+
+
+class reporter:
+
+    def __init__(self,title,link,disdate):
+        self.title = title
+        self.link = link
+        self.date = disdate
+ 
+ 
 for item in soup.findAll('item'):
     for t in item.findAll(['title', 'link', 'mlb:display-date']):
         articles.append(str(t.text))
@@ -28,25 +41,15 @@ while i<len(articles):
     i+=3
 for date in var_list:
     date[2] = datetime.datetime.strptime(date[2][:-4], '%m/%d/%Y %I:%M ')
-
-class reporter:
-
-    def __init__(self,title,link,disdate):
-        self.title = title
-        self.link = link
-        self.date = disdate
         
 for x in var_list:
 	obj = reporter( x[0], x[1], x[2])
 	obj_list.append(obj)
-	
-final_articles = []
 
 for i in obj_list:
 	if i.date >= yesterday:
 		final_articles.append(i.link)
-		
-print(len(final_articles))
+#########################################################
 
 
 
