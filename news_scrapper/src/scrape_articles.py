7d44b5943a1bb235ae06970830e9df05b3cd7a57
plouto5 <ploutos66p@gmail.com>
2017-06-09 17:22:13 +0000

    created scrape_articles.py

---
+++ news_scrapper/src/scrape_articles.py
@@ -0,0 +1,42 @@
+import urllib.requests
+from bs4 import BeautifulSoup
+
+def make_soup(url):
+    page = urllib.request.urlopen(url)
+    soupdata = BeautifulSoup(page, "lxml")
+    return soupdata
+
+def parse_site(data):
+    for title in data.find_all('title'):
+        print(title.text)
+    for author in data.find_all('span', 'author'):
+        print(author.text+ "\n")
+    for paragraph in data.find_all('p'):
+        print(paragraph.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
